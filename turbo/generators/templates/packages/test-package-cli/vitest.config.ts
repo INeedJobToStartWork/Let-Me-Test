ae0d7ff5fa3c0f85a4b57cd9da604c2ea515bf76
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2026-07-29 20:49:52 +0200

    ⚙️  Chore(generators): update typescript version and config

--- turbo/generators/templates/packages/test-package-cli/vitest.config.ts
+++ turbo/generators/templates/packages/test-package-cli/vitest.config.ts
@@ -2,6 +2,7 @@ import { resolve } from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	test: { passWithNoTests: true },
 	resolve: {
 		alias: {
 			"@": resolve(import.meta.dirname, "src/")
